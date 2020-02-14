9ac0a7552ee6468fb1153e074938ff2e90f295da
krosac <z1557340316@gmail.com>
2020-02-13 17:13:45 -0800

    Update build_module.cc

--- src/relay/backend/build_module.cc
+++ src/relay/backend/build_module.cc
@@ -334,7 +334,7 @@ class RelayBuildModule : public runtime::ModuleNode {
     pass_seqs.push_back(transform::CombineParallelConv2D(3));
     pass_seqs.push_back(transform::CombineParallelDense(3));
     pass_seqs.push_back(transform::FoldConstant());
-    //pass_seqs.push_back(transform::FoldScaleAxis());
+    pass_seqs.push_back(transform::FoldScaleAxis());
     pass_seqs.push_back(transform::CanonicalizeCast());
     pass_seqs.push_back(transform::CanonicalizeOps());
     //pass_seqs.push_back(transform::AmorphGraph());
